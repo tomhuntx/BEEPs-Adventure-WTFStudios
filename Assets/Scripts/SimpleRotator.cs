06f3162f40f928eadb1d55173248263c31df1d0c
Dwight Angelo Burbon <dj_burbz_360@yahoo.com>
2020-08-08 20:05:41 +1000

    implemented simple main menu background

---
+++ Assets/Scripts/SimpleRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotator : MonoBehaviour
+{
+    [SerializeField] private bool enableRotation = true;
+    [SerializeField] private float rotationSpeed = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(this.transform.up, rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Tools/CubeMapGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteAlways]
+[RequireComponent(typeof(Camera))]
+public class CubeMapGenerator : MonoBehaviour
+{
+    [Tooltip("The cubemap to be exported to.")]
+    [SerializeField] private Cubemap output;
+
+    public void GenerateCubeMap()
+    {
+        if (output != null)
+        {
+            this.GetComponent<Camera>().RenderToCubemap(output);
+            Debug.Log("Cubemap Exported!");
+        }
+        else
+        {
+            Debug.LogError("Output target is empty!");
+        }
+    }
+}
+
+
+[CustomEditor(typeof(CubeMapGenerator))]
+public class CubeMapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        CubeMapGenerator generator = (CubeMapGenerator) target;
+        if (GUILayout.Button ("Generate Cube Map"))
+        {
+            generator.GenerateCubeMap();
+        }
+    }
+}
