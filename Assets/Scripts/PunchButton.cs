d4cc096ba90bcefe7d123098751f190659513971
Tom Hunter <thenimblade@gmail.com>
2020-08-12 12:20:01 +1000

    Button

---
+++ Assets/Scripts/PunchButton.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchButton : MonoBehaviour
+{
+	private Animator anim;
+	private SpawnerRemote spawner;
+	public GameObject punchSFX;
+
+	public GameObject chuteTrigger;
+	public GameObject toSpawn;
+	private TriggerInside boxDetection;
+	private SpawnerRemote chuteSpawner;
+
+	private bool punched = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		anim = GetComponent<Animator>();
+		spawner = GetComponent<SpawnerRemote>();
+		chuteSpawner = chuteTrigger.GetComponent<SpawnerRemote>();
+		boxDetection = chuteTrigger.GetComponent<TriggerInside>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (punched)
+		{
+
+		}
+    }
+
+	IEnumerator Cooldown()
+	{
+		yield return new WaitForSeconds(2);
+
+		punched = false;
+	}
+
+	IEnumerator PauseThenSpawn()
+	{
+		yield return new WaitForSeconds(0.5f);
+
+		if (!boxDetection.inside)
+		{
+			chuteSpawner.SpawnToPoint(toSpawn);
+
+		}
+	}
+
+	public void Punched()
+	{
+		if (!punched)
+		{
+			punched = true;
+			anim.Play("button");
+
+			spawner.SpawnToPoint(punchSFX);
+
+			StartCoroutine(PauseThenSpawn());
+			StartCoroutine(Cooldown());
+
+			Debug.Log("I have been punched!");
+		}
+	}
+}
---
+++ Assets/Scripts/TriggerInside.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerInside : MonoBehaviour
+{
+	public bool inside; // is there a box in the collider?
+	private bool checking = false; //only check once at a time
+
+	IEnumerator Check()
+	{
+		checking = true;
+
+		yield return new WaitForSeconds(1);
+
+		inside = false;
+		checking = false;
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Box")
+		{
+			inside = true;
+			if (!checking)
+			{
+				StartCoroutine(Check());
+			}
+		}
+	}
+}
