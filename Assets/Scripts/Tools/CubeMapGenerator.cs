02f235888c5edf99fa5d655700239a7782c3a76b
Dwight Angelo Burbon <dj_burbz_360@yahoo.com>
2020-08-14 02:51:43 +1000

    made the CubeMapGenerator.cs only run in editor

--- Assets/Scripts/Tools/CubeMapGenerator.cs
+++ Assets/Scripts/Tools/CubeMapGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -38,3 +39,4 @@ public class CubeMapGeneratorEditor : Editor
         }
     }
 }
+#endif
