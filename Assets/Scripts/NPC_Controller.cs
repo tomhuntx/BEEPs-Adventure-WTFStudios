f7b47176aadfa7fbd3ab5c8a1615ac135f0bbc76
Tom Hunter <thenimblade@gmail.com>
2020-08-19 11:46:59 +1000

    sorting NPCs WIP

--- Assets/Scripts/NPC_Controller.cs
+++ Assets/Scripts/NPC_Controller.cs
@@ -5,39 +5,81 @@ using UnityEngine.AI;
 
 public class NPC_Controller : MonoBehaviour
 {
-	[Tooltip("Points to patrol between (in order from first to last, then back to first, and so on).")]
-	public GameObject[] patrolPoints;
+	[Tooltip("Parent gameobject with every RIGHT move point within it.")]
+	public GameObject moveParentR;
+	[Tooltip("Parent gameobject with every LEFT move point within it.")]
+	public GameObject moveParentL;
+	private List<GameObject> movePointsR;
+	private List<GameObject> movePointsL;
 
 	private NavMeshAgent agent;
 	private int currentPoint = 0;
+	private int newPoint = 0;
+	bool moveLeft;
 
 	private void Start()
 	{
-		if (patrolPoints.Length == 0)
+		movePointsR = new List<GameObject> { };
+		movePointsL = new List<GameObject> { };
+
+		// Get and store all the children
+		Transform[] allChildrenR = moveParentR.GetComponentsInChildren<Transform>();
+		foreach (Transform child in allChildrenR)
 		{
-			Debug.LogError("Please add at least one patrol point to this bot.");
+			movePointsR.Add(child.gameObject);
+		}
+		Transform[] allChildrenL = moveParentL.GetComponentsInChildren<Transform>();
+		foreach (Transform child in allChildrenL)
+		{
+			movePointsL.Add(child.gameObject);
+		}
+
+		// Check this worked
+		if (movePointsR.Count == 0 || movePointsL.Count == 0)
+		{
+			Debug.LogError("Please add at the point parents to this bot.");
 		}
 		agent = GetComponent<NavMeshAgent>();
+
+		
+
+		// Randomly start with left or right points
+		bool moveLeft = (Random.value > 0.5f);
+
+		// Get a random point to start with
+		if (moveLeft)
+			currentPoint = Random.Range(0, movePointsL.Count);
+		else
+			currentPoint = Random.Range(0, movePointsR.Count);
 	}
 
 	void Update()
 	{
+		/* HAVE TO BRB - WILL FIX
 		// If not reached location, move to it
-		if (Vector3.Distance(transform.position, patrolPoints[currentPoint].transform.position) > 2.0f)
+		if (Vector3.Distance(transform.position, movePoints[currentPoint].transform.position) > 2.0f)
 		{
-			agent.SetDestination(patrolPoints[currentPoint].transform.position);
+			agent.SetDestination(movePoints[currentPoint].transform.position);
 		}
 		// Otherwise, wait then move to the next patrol point
 		else
 		{
-			StartCoroutine(Wait(1));
+			StartCoroutine(Wait(2));
 
-			currentPoint++;
-			if (currentPoint == patrolPoints.Length)
+			// Get a new random point
+			newPoint = Random.Range(0, movePoints.Count);
+
+			// Make it increadibly unlikely to get the same point twice
+			if (newPoint == currentPoint)
 			{
-				currentPoint = 0;
+				currentPoint = Random.Range(0, movePoints.Count);
+			}
+			else
+			{
+				currentPoint = newPoint;
 			}
 		}
+		*/
 	}
 
 	private IEnumerator Wait(float seconds)
