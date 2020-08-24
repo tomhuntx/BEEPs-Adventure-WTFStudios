fea685f31b8b56654a5fb5e5e349157a30f45186
Tom Hunter <thenimblade@gmail.com>
2020-08-24 19:01:27 +1000

    1/2 Mbot Animations

--- Assets/Scripts/MBot_Controller.cs
+++ Assets/Scripts/MBot_Controller.cs
@@ -6,7 +6,6 @@ using UnityEngine.Events;
 
 public class MBot_Controller : MonoBehaviour
 {
-
 	private Vector3 startPosition;
 	private NavMeshAgent agent;
 
@@ -18,6 +17,11 @@ public class MBot_Controller : MonoBehaviour
 	// Events
 	public UnityEvent onDisturb;
 
+	// The Bot's Animator (all 3 are required unfortunately)
+	public Animator anim;
+	public Animator animFace;
+	public Animator animScreen;
+
 	private bool lookingForHat = false;
 	private bool nearHat = true;
 
@@ -26,6 +30,7 @@ public class MBot_Controller : MonoBehaviour
     {
 		startPosition = transform.position;
 		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(startPosition);
 	}
 
     // Update is called once per frame
@@ -50,28 +55,39 @@ public class MBot_Controller : MonoBehaviour
 			nearHat = false;
 			agent.SetDestination(hardhat.transform.position);
 		}
+
+
+		if (!lookingForHat && Vector3.Distance(transform.position, startPosition) < 0.1f)
+		{
+			anim.SetTrigger("backHome");
+			animFace.SetTrigger("backHome");
+			animScreen.SetTrigger("backHome");
+		}
     }
 
 	public void HatMoved()
 	{
 		lookingForHat = true;
 		agent.SetDestination(hardhat.transform.position);
+		anim.SetTrigger("hatGrabbed");
+		animFace.SetTrigger("hatGrabbed");
+		animScreen.SetTrigger("hatGrabbed");
 
 		// Stop interact animation
 	}
 
 	private void RotateTo(Vector3 target)
 	{
+		GameObject child = this.transform.GetChild(0).gameObject;
 		Vector3 direction = (target - transform.position).normalized;
 		Quaternion lookRotation = Quaternion.LookRotation(direction);
-		transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 2f);
+		child.transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 2f);
 	}
 
 	private void RotateTo(Quaternion target)
 	{
-		Vector3 direction = transform.position.normalized;
-		Quaternion lookRotation = Quaternion.LookRotation(direction);
-		transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 2f);
+		GameObject child = this.transform.GetChild(0).gameObject;
+		child.transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 2f);
 	}
 
 	private void TryGrabHat()
@@ -84,7 +100,7 @@ public class MBot_Controller : MonoBehaviour
 			// Stop agent before waiting
 			agent.isStopped = true;
 
-			StartCoroutine(GrabPause(2.5f));
+			StartCoroutine(GrabPause(2.0f));
 		}
 	}
 
@@ -114,5 +130,4 @@ public class MBot_Controller : MonoBehaviour
 			GrabHat();
 		}
 	}
-
 }
