65c91b3a57c3c5a77192979214a263b6146bd81d
Dwight Angelo Burbon <dj_burbz_360@yahoo.com>
2020-09-23 23:46:56 +1000

    Forces negated on player squish

--- Assets/Scripts/PlayerCharacterController.cs
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -693,8 +693,8 @@ public class PlayerCharacterController : MonoBehaviour
         externalForce = Vector3.zero;
         velocity = Vector3.zero;
         rb.velocity = Vector3.zero;
-        rb.inertiaTensor = Vector3.zero;
-        rb.inertiaTensorRotation = Quaternion.identity;
+        //rb.inertiaTensor = Vector3.zero;
+        //rb.inertiaTensorRotation = Quaternion.identity;
     }
     #endregion
 }
