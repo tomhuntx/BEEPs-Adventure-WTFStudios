c0b17d514a50718875166db256381ccc1a38a5ec
Tom Hunter <thenimblade@gmail.com>
2020-10-14 18:56:48 +1000

    Implemented MBot controls and misc bugfixes

---
+++ Assets/Scripts/LeverController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverController : MonoBehaviour
+{
+	public Animator lever1;
+	public Animator lever2;
+
+	public void SetBools(bool on)
+	{
+		lever1.SetBool("managerOnPodium", on);
+		lever2.SetBool("managerOnPodium", on);
+	}
+
+	public void SetSpeed(float speed)
+	{
+		lever1.speed = speed;
+		lever2.speed = speed;
+	}
+}
